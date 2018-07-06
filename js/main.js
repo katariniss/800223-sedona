e82b03f6d65642eba8f97c8c12337c6a28b9df32
Katariniss <echekina@gmail.com>
2018-07-06 14:33:07 +0500

    добавлен функционал счетчика для количества взрослых и детей

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ var form = searchPopup.querySelector("form");
 
 var arrivalDate = searchPopup.querySelector("[name=arrival-date]");
 var departureDate = searchPopup.querySelector("[name=departure-date]");
-var adultsNumber = searchPopup.querySelector("[name=adults-number]");
-var childrenNumber = searchPopup.querySelector("[name=children-number]");
+var adultsNumberInput = searchPopup.querySelector("[name=adults-number]");
+var childrenNumberInput = searchPopup.querySelector("[name=children-number]");
 
 var isStorageSupported = true;
 var adultsFromStorage = "";
 var childrenFromStorage = "";
 
 try {
-    adultsFromStorage = localStorage.getItem("adultsNumber");
-    childrenFromStorage = localStorage.getItem("childrenNumber");
+    adultsFromStorage = localStorage.getItem("adultsNumberInput");
+    childrenFromStorage = localStorage.getItem("childrenNumberInput");
 } catch (err) {
     isStorageSupported = false;
 }
@@ -24,15 +24,15 @@ searchButton.addEventListener('click', function (evt) {
     searchPopup.classList.toggle('modal-show');
 
     if (adultsFromStorage) {
-        adultsNumber.value = adultsFromStorage;
+        adultsNumberInput.value = adultsFromStorage;
     }
     if (childrenFromStorage) {
-        childrenNumber.value = childrenFromStorage;
+        childrenNumberInput.value = childrenFromStorage;
     }
 });
 
 form.addEventListener("submit", function (evt) {
-    if (!arrivalDate.value || !departureDate.value || !adultsNumber.value) {
+    if (!arrivalDate.value || !departureDate.value || !adultsNumberInput.value) {
         evt.preventDefault();
 
         searchPopup.classList.remove("modal-error");
@@ -40,10 +40,46 @@ form.addEventListener("submit", function (evt) {
         searchPopup.classList.add("modal-error");
     } else {
         if (isStorageSupported) {
-            localStorage.setItem("adultsNumber", adultsNumber.value);
-            localStorage.setItem("childrenNumber", childrenNumber.value);
+            localStorage.setItem("adultsNumberInput", adultsNumberInput.value);
+            localStorage.setItem("childrenNumberInput", childrenNumberInput.value);
         }
     }
 });
 
+var adultsIncrementButton = document.querySelector('.adults-plus-button');
+var adultsDecrementButton = document.querySelector('.adults-minus-button');
 
+var childrenIncrementButton = document.querySelector('.children-plus-button');
+var childrenDecrementButton = document.querySelector('.children-minus-button');
+
+adultsIncrementButton.addEventListener('click', function (evt) {
+    incrementInputValue(adultsNumberInput);
+});
+
+adultsDecrementButton.addEventListener('click', function (evt) {
+    decrementInputValue(adultsNumberInput);
+});
+
+
+childrenIncrementButton.addEventListener('click', function (evt) {
+    incrementInputValue(childrenNumberInput);
+});
+
+childrenDecrementButton.addEventListener('click', function (evt) {
+    decrementInputValue(childrenNumberInput);
+});
+
+
+function incrementInputValue(inputElement){
+    inputElement.value = Number(inputElement.value) + 1;
+}
+
+function decrementInputValue(inputElement){
+    var valueAsNumber = Number(inputElement.value);
+
+    if (valueAsNumber === 0) {
+        return;
+    }
+
+    inputElement.value = valueAsNumber - 1;
+}
